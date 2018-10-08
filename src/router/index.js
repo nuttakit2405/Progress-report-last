d461add468cdb861384a566bda145d69dc6fd9cc
nuttakit2405 <5806021631033@fitm.kmutnb.ac.th>
2018-10-08 18:40:23 +0700

    interface-V1

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import HomeStudent from '@/components/HomeStudent'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/homestudent',
+      name: 'HomeStudent',
+      component: HomeStudent
     }
   ]
 })
