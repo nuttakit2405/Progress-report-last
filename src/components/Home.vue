64bddb6f4599188f6826cb7624251c7e2d41b04f
nuttakit2405 <5806021631033@fitm.kmutnb.ac.th>
2018-11-01 15:14:52 +0700

    โหมดของอาจารย์

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,37 +2,13 @@
   <div class="hello">
     <div class="container">
       <div class="column">
-        <div class="column is-full">
-          <div class="level" >
-            <section>
-              <div class="level">
-                <b-collapse class="card" v-for="ins in 5" :key="ins">
-                  <div slot="trigger" slot-scope="props" class="card-header">
-                      <p class="card-header-title">
-                          Component
-                      </p>
-                      <a class="card-header-icon">
-                          <b-icon
-                              :icon="props.open ? 'menu-down' : 'menu-up'">
-                          </b-icon>
-                      </a>
-                  </div>
-                  <div class="card-content">
-                      <div class="content">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                          <a>#buefy</a>.
-                      </div>
-                  </div>
-                  <footer class="card-footer">
-                      <a class="card-footer-item">Save</a>
-                      <a class="card-footer-item">Edit</a>
-                      <a class="card-footer-item">Delete</a>
-                  </footer>
-                </b-collapse>
-              </div>
-            </section>
+        <div class="notification">
+          <div class="level">
+            <div class="level-item has-text-centered ">
+              <router-link :to="{name: 'Login'}" class="button is-medium">Login</router-link>
+            </div>
           </div>
-        </div>
+        </div> 
       </div>
     </div>
   </div>
