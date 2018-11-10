8db67be06d2c6e5d5b6255e30da250a075f8a848
kanyaruk <ahmkanyaruk@hotmail.com>
2018-11-11 01:25:13 +0700

    feat: can upload to db but not response after upload

--- src/views/admin/ImportStudent.vue
+++ src/views/admin/ImportStudent.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
-    <b-field class="file">
-      <b-upload v-model="file" :accept="fileAccept.join(',')">
-        <a class="button is-primary">
-          <b-icon icon="upload"></b-icon>
-          <span>Click to upload</span>
-        </a>
-      </b-upload>
-      <span class="file-name" v-if="file">
-        {{ file.name }}
-      </span>
-    </b-field>
-    <button class="button" @click="parse(file)">view data</button>
+    <div class="column"></div>
+    <div class="columns column is-centered">
+      <b-field class="file">
+        <b-upload v-model="file" :accept="fileAccept.join(',')" @input="parse(file)">
+          <a class="button is-primary">
+            <b-icon icon="upload"></b-icon>
+            <span>เลือกไฟล์</span>
+          </a>
+        </b-upload>
+        <span class="file-name" v-if="file">
+          {{ file.name }}
+        </span>
+      </b-field>
+    </div>
+    <!-- <button class="button" @click="parse(file)">view data</button> -->
     <!-- <b-table v-if="student" :data="student" :columns="columns"></b-table> -->
     <table class="table" v-if="student">
+      <tr>
+        <td :key="ic" v-for="(column, ic) in maxColumn">
+          <b-field v-if="ic !== 0">
+            <b-select v-model="columnSelected[ic]" @input="selectedField($event, ic)" placeholder="Select a field">
+              <option
+                  v-for="(option, key) in selectOptions"
+                  :value="option.value"
+                  :key="key"
+                  :disabled="option.seletcted && option.seletcted !== ic">
+                  {{ option.title }}
+              </option>
+            </b-select>
+          </b-field>
+          <div v-else>
+            <b-checkbox @input="selectAll(checkAll)" v-model="checkAll">เลือกทั้งหมด</b-checkbox>
+          </div>
+        </td>
+      </tr>
       <tr :key="ir" v-for="(row, ir) in student">
+        <td class="has-text-centered"><b-checkbox :native-value="ir" v-model="rowSelected"></b-checkbox></td>
         <td :key="ic" v-for="(column, ic) in row">{{column}}</td>
       </tr>
     </table>
+    <div v-if="student" class="columns column is-centered">
+      <button class="button" @click="saveToDB">บันทึกข้อมูล</button>
+    </div>
   </div>
 </template>
 
 <script>
+import db from '@/database'
 import XLSX from 'xlsx'
 var reader = new FileReader()
 export default {
@@ -34,10 +60,41 @@ export default {
       ],
       file: null,
       student: null,
-      columns: null
+      columns: null,
+      maxColumn: 0,
+      selectOptions: [
+        {title: '(ยังไม่ได้เลือก)', value: 'disabled', seletcted: null},
+        {title: 'รหัสนักศึกษา', value: 'id', seletcted: null},
+        {title: 'ชื่อ', value: 'name', seletcted: null},
+        {title: 'นามสกุล', value: 'lastname', seletcted: null},
+        {title: 'สาขา', value: 'branch', seletcted: null}
+      ],
+      rowSelected: [],
+      columnSelected: [],
+      checkAll: false
     }
   },
   methods: {
+    selectedField (e, column) {
+      const oldIndex = this.selectOptions.findIndex(v => v.seletcted === column)
+      if (oldIndex !== -1) {
+        this.selectOptions[oldIndex].seletcted = null
+      }
+      if (e === 'disabled') {
+        return
+      }
+      const index = this.selectOptions.findIndex(v => v.value === e)
+      if (index !== -1) {
+        this.selectOptions[index].seletcted = column
+      }
+    },
+    selectAll (flag) {
+      if (flag) {
+        this.rowSelected = new Array(this.student.length).fill(0).map((v, i) => i)
+      } else {
+        this.rowSelected = []
+      }
+    },
     async parse (file) {
       const res = await this.loadBinaryFile(file)
       const wb = XLSX.read(res, {type: 'binary'})
@@ -46,6 +103,7 @@ export default {
       const data = XLSX.utils.sheet_to_json(ws, {header: 1})
       const allLength = data.map(val => val.length)
       const max = Math.max(...allLength)
+      this.maxColumn = max
       this.columns = new Array(max).fill(1).map((val, i) => {
         return {
           field: i,
@@ -59,6 +117,7 @@ export default {
         }, {})
         return inner
       })
+      this.columnSelected = new Array(max).fill(0).map((v) => 'disabled')
     },
     loadBinaryFile (file) {
       return new Promise(function (resolve, reject) {
@@ -67,6 +126,26 @@ export default {
         }, false)
         reader.readAsBinaryString(file)
       })
+    },
+    async saveToDB () {
+      const idColumn = this.columnSelected.findIndex(v => v === 'id') - 1
+      const nameColumn = this.columnSelected.findIndex(v => v === 'name') - 1
+      const lastnameColumn = this.columnSelected.findIndex(v => v === 'lastname') - 1
+      const branchColumn = this.columnSelected.findIndex(v => v === 'branch') - 1
+
+      const data = this.rowSelected.map(row => {
+        return {
+          id: this.student[row][idColumn].replace(/-/g, ''),
+          name: this.student[row][nameColumn],
+          lastname: this.student[row][lastnameColumn],
+          branch: this.student[row][branchColumn]
+        }
+      }).reduce((prev, curr) => {
+        prev[curr.id] = curr
+        return prev
+      }, {})
+      const res = await db.database.ref('students').update(data)
+      console.log(res)
     }
   }
 }
