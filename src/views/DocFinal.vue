37c1919dd2c2346c93d05caa98b1b0efc5e46c5c
kanyaruk <ahmkanyaruk@hotmail.com>
2018-11-23 00:52:13 +0700

    docfinal

---
+++ src/views/DocFinal.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <div class="download">
+      <button class="button is-info" >ดาวน์โหลด</button>
+    </div>
+    <img src="/static/img/ใบยื่นสอบ.jpg"><br>
+</div>
+</template>
+<style scoped>
+.download{
+  margin-top: 20px;
+  margin-right: 100px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
