7f498cd493b4b3d06d91d677fab53c2a38f09d91
kanyaruk <ahmkanyaruk@hotmail.com>
2018-12-24 23:40:47 +0700

    re style

--- src/components/Group.vue
+++ src/components/Group.vue
@@ -4,7 +4,9 @@
       <div class="card">
         <header class="card-header" style="position: relative; width: 100%; padding: 10px;">
           <p class="card-header-title" style="padding-left: 5px;">
-            <span class="dotdotdot" :title="data.thaiProjectName">{{data.thaiProjectName}}</span>
+            <span @click="$router.push({name: 'ProjectScoreBoard', params: {projectId: projectId}})"
+              class="dotdotdot project-name"
+              :title="data.thaiProjectName">{{data.thaiProjectName}}</span>
           </p>
           <div style="display: flex; flex-direction: row;">
             <button class="button" @click="$router.push({name: 'CalendarProject', params: {projectId: projectId}})">
@@ -18,7 +20,7 @@
               <button class="button" slot="trigger">
                   <i class="fas fa-ellipsis-v"></i>
               </button>
-              <b-dropdown-item @click="$emit('edit', projectId)">
+              <b-dropdown-item v-if="data.approveSpecialProject !== true"  @click="$emit('edit', projectId)">
                 <i class="fas fa-edit"></i>&nbsp;<span>แก้ไขโปรเจค</span>
               </b-dropdown-item>
               <b-dropdown-item @click="$emit('remove', projectId)">
@@ -29,17 +31,19 @@
         </header>
         <div class="card-content">
           <div class="columns" style="align-items: center">
-            <div class="column is-paddingless"><progress class="progress" :class="[ data.progress == 100 ? 'is-success' : 'is-info']" :value="data.progress ? data.progress : 0" max="100"></progress></div>
+            <div class="column is-paddingless"><progress class="progress" :class="[ data.approveSpecialProject ? 'is-success' : data.approveSpecialProject === false ? 'is-danger' : 'is-info']" :value="data.progress ? data.progress : 0" max="100"></progress></div>
             <span class="column is-paddingless" style="flex: none;width: fit-content;">&nbsp;{{data.progress ? data.progress : 0}}%</span>
           </div>
           <!-- {{data}} -->
           <div>
-            วันทำงานตั้งแต่
+            <b>วันทำงานตั้งแต่</b><br>
             {{$dayjs(data.startProject).format('DD-MMM-YYYY')}}
-            ถึง
+            <b>ถึง</b>
             {{$dayjs(data.deadlineProject).format('DD-MMM-YYYY')}}
           </div>
+            <b>อาจารย์ที่ปรึกษา:</b><br>
            {{data.mentor.position}}{{data.mentor.name | removeTitle}} {{data.mentor.lastname}} <br>
+           <b>สมาชิก:</b><br>
           <span :key="member.id" v-for="member in data.teams">
             {{member.id}} {{member.name}} {{member.lastname}} <br>
           </span>
@@ -49,36 +53,24 @@
         </div>
         <footer class="card-footer">
           <p class="card-footer-item">
-              <span>
-              <b-icon
-                  icon="video"
-                  size="is-meduim">
-              </b-icon>
+              <span class="icon fa-lg icon-hover" >
+                <i class="fa fa-video"></i>
               </span>
           </p>
           <p class="card-footer-item">
-              <span>
-              <b-icon
-                  icon="comments"
-                  size="is-meduim">
-              </b-icon>
+              <span class="icon fa-lg icon-hover" >
+                <i class="fa fa-comments"></i>
               </span>
           </p>
           <p class="card-footer-item" style="cursor: pointer;" @click="$router.push({name: 'ProjectScoreBoard', params: {projectId: projectId}})">
-              <span>
-              <b-icon
-                  icon="file-alt"
-                  size="is-meduim">
-              </b-icon>
+              <span class="icon fa-lg icon-hover" >
+                <i class="fa fa-file-alt"></i>
               </span>
           </p>
           <p class="card-footer-item"  style="cursor: pointer;" @click="$router.push({name: 'ProjectFolder', params: {projectId: projectId}})">
-            <span>
-              <b-icon
-                  icon="folder"
-                  size="is-meduim">
-              </b-icon>
-            </span>
+              <span class="icon fa-lg icon-hover" >
+                <i class="fas fa-folder-open"></i>
+              </span>
           </p>
         </footer>
       </div>
@@ -116,12 +108,15 @@ export default {
 }
 </script>
 <style scooped>
-/* .layoutcalendar{
-  margin-top:50px;
-  margin-left: auto;
-  margin-right: auto;
-  width:450px;
-} */
+.project-name:hover {
+  cursor: pointer;
+  color: hsl(204, 86%, 53%);
+}
+
+.icon-hover:hover {
+  cursor: pointer;
+  color: hsl(204, 86%, 53%);
+}
 .sizeCard {
   background: #eee;
   max-width: 900px;
