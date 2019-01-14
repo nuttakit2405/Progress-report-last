c2e2ea3c4735215dd8f2a6f21477c4d92539366c
kanyaruk <ahmkanyaruk@hotmail.com>
2019-01-14 22:32:24 +0700

    auto response

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -1,32 +1,34 @@
 <template>
   <div style="width: 100%; padding-top: 20px;" align="center">
-    <a href="https://chrome.google.com/webstore/detail/screen-capturing/ajhifddimkapgcifgcodmmfdlknahffk">Install Chrome extension</a>
+    <!-- <a href="https://chrome.google.com/webstore/detail/screen-capturing/ajhifddimkapgcifgcodmmfdlknahffk">Install Chrome extension</a> -->
     <div style="display: flex; width: 100%; justify-content: center; ">
       <div class="vid">
         <span>Remote</span><br>
-        <video class="video" id="remoteVideo" autoplay></video>
+        <video class="video remote" id="remoteVideo" autoplay></video>
       </div>
       <div class="vid" v-if="showScreen">
         <span>Screen</span><br>
         <video class="video" id="screen" autoplay muted></video>
       </div>
-      <div class="vid">
+      <div class="vid local">
         <span>Local</span><br>
-        <video class="video" id="localVideo" autoplay muted></video>
-        <div>
-          <button class="button" @click="toggleVideo()">
-            <span v-show="onVideo"><i class="fas fa-video"></i></span>
-            <span v-show="!onVideo"><i class="fas fa-video-slash"></i></span>
-          </button>
-          <button class="button" @click="toggleMicrophone()">
-            <span v-show="onMicrophone"><b-icon icon="microphone-alt"></b-icon></span>
-            <span v-show="!onMicrophone"><b-icon icon="microphone-alt-slash"></b-icon></span>
-          </button>
+        <div class="local">
+          <video class="video" id="localVideo" autoplay muted></video>
+          <!-- <div class="option">
+            <button class="button" @click="toggleVideo()">
+              <span v-show="onVideo"><i class="fas fa-video"></i></span>
+              <span v-show="!onVideo"><i class="fas fa-video-slash"></i></span>
+            </button>
+            <button class="button" @click="toggleMicrophone()">
+              <span v-show="onMicrophone"><b-icon icon="microphone-alt"></b-icon></span>
+              <span v-show="!onMicrophone"><b-icon icon="microphone-alt-slash"></b-icon></span>
+            </button>
+          </div> -->
         </div>
       </div>
     </div>
     <button class="button" @click="share">Share Screen</button>
-    <button class="button" @click="stream">Start Stream</button>
+    <!-- <button class="button" @click="stream">Start Stream</button> -->
   </div>
 </template>
 
@@ -75,7 +77,9 @@ export default {
   },
   async created () {
     // webrtc.droneOpen(this.roomHash)
-    await webrtc.openLocalVideo(this.onVideo, this.onMicrophone)
+    // await webrtc.openLocalVideo(this.onVideo, this.onMicrophone)
+  },
+  async mounted () {
     await webrtc.droneOpen(this.projectId)
   },
   beforeDestroy () {
@@ -90,8 +94,27 @@ border-radius: 6px;
 padding: 1px;
 width: 500px;
 background-color: black;
+-webkit-transform: scaleX(-1);
+transform: scaleX(-1);
 }
 .vid {
   margin: 10px;
+}
+.remote {
+}
+.local {
+  position: relative;
+  z-index: -1;
+}
+
+.option {
+  top: 0;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
 }
 </style>
