11126300cae0ea5da044bde60063128bf5c05cd0
TonPC64 <kingkong2103@gmail.com>
2019-05-31 03:10:35 +0700

    move to comp

---
+++ src/components/CalendarDate.vue
@@ -0,0 +1,55 @@
+<template>
+<div >
+  <div class="columns is-marginless mg-bt-2px" :key="event.key" v-for="event in ee">
+    <div class="column is-paddingless" style="width: 100%; position: relative;">
+      <span style="width: 100%; padding: 0 2px" :class="['dotdotdot', event.waitaccept ? 'disable-events': 'accept-events' ]" @click="viewEvent(item.date.full, key, event)" :title="event.title">{{event.title}}</span>
+      </div>
+
+      <div class="column is-paddingless" style="flex: none; width: fit-content;">
+      <button
+        v-if="!(!item.isCurMonth || today > item.date.date) && event.waitaccept"
+        class="button action-btn is-small"
+        @click="editEvent(item.date.year, item.date.full, event.key, event)"
+      >
+        <b-icon size="is-small" icon="edit"/>
+      </button>
+      </div>
+
+      <div class="column is-paddingless" style="flex: none; width: fit-content;">
+      <button class="button action-btn is-small" @click="removeEvent(item.date.full, event.key, event)">
+        <b-icon size="is-small" icon="times"/>
+      </button>
+    <!-- <button v-if="!(!item.isCurMonth || today > item.date.date)" class="button is-small" @click="removeEvent(item.date.full, key, event)"><b-icon size="is-small" icon="times"/></button> -->
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['item', 'ee', 'viewEvent', 'removeEvent', 'editEvent', 'today']
+}
+</script>
+
+<style scoped>
+.dotdotdot {
+  cursor: pointer;
+  display: inline-block !important;
+  white-space: nowrap !important;
+  overflow: hidden !important;
+  text-overflow: ellipsis !important;
+}
+.accept-events {
+  background-color: aquamarine;
+}
+.disable-events {
+  background-color: lightgray;
+}
+
+.action-btn {
+  width: 25px;
+  height: 24px;
+  padding: 3px;
+  margin-left: 1px;
+}
+</style>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -30,56 +30,14 @@
               <!-- event -->
               <div class="pd-hrzt-3px" v-if="events[item.date.full]">
                 <div :key="k" v-for="(ee, k) in eventToArr(events[item.date.full])">
-                  <div class="columns is-marginless mg-bt-2px" :key="event.key" v-for="event in ee.part1">
-                    <div class="column is-paddingless" style="width: 100%; position: relative;">
-                      <span style="width: 100%; padding: 0 2px" :class="['dotdotdot', event.waitaccept ? 'disable-events': 'accept-events' ]" @click="viewEvent(item.date.full, key, event)" :title="event.title">{{event.title}}</span>
-                    </div>
-
-                    <div class="column is-paddingless" style="flex: none; width: fit-content;">
-                      <button
-                        v-if="!(!item.isCurMonth || today > item.date.date) && event.waitaccept"
-                        class="button action-btn is-small"
-                        @click="editEvent(item.date.year, item.date.full, event.key, event)"
-                      >
-                        <b-icon size="is-small" icon="edit"/>
-                      </button>
-                    </div>
-
-                    <div class="column is-paddingless" style="flex: none; width: fit-content;">
-                      <button class="button action-btn is-small" @click="removeEvent(item.date.full, event.key, event)">
-                        <b-icon size="is-small" icon="times"/>
-                      </button>
-                      <!-- <button v-if="!(!item.isCurMonth || today > item.date.date)" class="button is-small" @click="removeEvent(item.date.full, key, event)"><b-icon size="is-small" icon="times"/></button> -->
-                    </div>
-                  </div>
+                  <CalendarDate :key="item.date.full+'part1'" :item="item" :ee="ee.part1" :viewEvent="viewEvent" :editEvent="editEvent" :removeEvent="removeEvent" :today="today" />
                   <b-dropdown hoverable aria-role="list" v-if="ee.part2">
                       <button class="button is-small more-btn" slot="trigger">
                           <span>{{ee.part2.length}} more</span>
                       </button>
 
-                      <b-dropdown-item aria-role="menu-item" custom class="pd-hrzt-4px pd-vtc-0px">
-                        <div class="columns is-marginless mg-bt-2px" :key="event.key" v-for="event in ee.part2">
-                          <div class="column is-paddingless" style="width: 100%; position: relative;">
-                            <span style="width: 100%; padding: 0 2px" :class="['dotdotdot', event.waitaccept ? 'disable-events': 'accept-events' ]" @click="viewEvent(item.date.full, key, event)" :title="event.title">{{event.title}}</span>
-                          </div>
-
-                          <div class="column is-paddingless" style="flex: none; width: fit-content;">
-                            <button
-                              v-if="!(!item.isCurMonth || today > item.date.date) && event.waitaccept"
-                              class="button action-btn is-small"
-                              @click="editEvent(item.date.year, item.date.full, event.key, event)"
-                            >
-                              <b-icon size="is-small" icon="edit"/>
-                            </button>
-                          </div>
-
-                          <div class="column is-paddingless" style="flex: none; width: fit-content;">
-                            <button class="button action-btn is-small" @click="removeEvent(item.date.full, event.key, event)">
-                              <b-icon size="is-small" icon="times"/>
-                            </button>
-                            <!-- <button v-if="!(!item.isCurMonth || today > item.date.date)" class="button is-small" @click="removeEvent(item.date.full, key, event)"><b-icon size="is-small" icon="times"/></button> -->
-                          </div>
-                        </div>
+                      <b-dropdown-item :key="item.date.full+'drop'" aria-role="menu-item" custom class="pd-hrzt-4px pd-vtc-0px">
+                        <CalendarDate :key="item.date.full+'part2'"  :item="item" :ee="ee.part2" :viewEvent="viewEvent" :editEvent="editEvent" :removeEvent="removeEvent" :today="today" />
                       </b-dropdown-item>
                   </b-dropdown>
                 </div>
@@ -94,8 +52,12 @@
 <script>
 import {mapGetters, mapActions} from 'vuex'
 import db from '@/database'
+import CalendarDate from '@/components/CalendarDate'
 
 export default {
+  components: {
+    CalendarDate
+  },
   props: {
     projectId: {
       type: String
@@ -625,19 +587,6 @@ export default {
   cursor: pointer;
   font-size: 14px;
 }
-.dotdotdot {
-  cursor: pointer;
-  display: inline-block !important;
-  white-space: nowrap !important;
-  overflow: hidden !important;
-  text-overflow: ellipsis !important;
-}
-.accept-events {
-  background-color: aquamarine;
-}
-.disable-events {
-  background-color: lightgray;
-}
 .is-not-curr-month {
   height: 100%;
   background-color: #f9f9f9;
@@ -658,14 +607,12 @@ export default {
   background: #e8ebee;
   color: black;
 }
-.action-btn {
-  width: 25px;
-  height: 24px;
-  padding: 3px;
-  margin-left: 1px;
-}
 .more-btn {
   border: 0px;
   background: #e8ebee;
+}
+
+.more-btn:hover {
+  background: #f9f9f9;
 }
 </style>
