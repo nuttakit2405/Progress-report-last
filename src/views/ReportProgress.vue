6848a3d068d4ca71e32850144dde0eb0cd37b26c
TonPC64 <kingkong2103@gmail.com>
2019-06-03 23:00:24 +0700

    5 row

--- src/views/ReportProgress.vue
+++ src/views/ReportProgress.vue
@@ -1,133 +1,129 @@
 <template>
   <div v-if="projectSelected">
     <div class="DownloadButton">
-      <!-- <b-loading :is-full-page="isFullPage" :active.sync="isLoading" :can-cancel="true"></b-loading> -->
-      <button class="button is-info DownloadButton" @click="printPDF(projectSelected.thaiProjectName)">
+      <button class="button is-info" @click="printPDF(projectSelected.thaiProjectName)">
           <b-icon icon="file-download"></b-icon> <span>ดาวโหลดไฟล์</span>
       </button>
     </div>
 
     <div style="display: flex; justify-content: center;">
-      <div id="frontBook" class="box LayoutFrame">
+      <div class="box bg-cl-white pd-40px mg-t-25px">
+        <div id="frontBook" class="LayoutFrame">
 
-      <div class="NameProject">ทก.02</div>
+          <div class="NameProject">ทก.02</div>
 
-      <div class="media logoKMUTNB" style="margin-top:50px;">
-        <figure>
-          <img src="/static/img/logoKMUTNB.JPG" width="200" alt="Image">
-        </figure>
-      </div>
+          <div class="media logoKMUTNB mg-t-50px">
+            <figure>
+              <img src="/static/img/logoKMUTNB.JPG" width="200" alt="Image">
+            </figure>
+          </div>
 
           <div class= "topTitle" >
-            <center><h2><b class="font" style="font-size: 25px;">แบบรายงานความก้าวหน้าของโครงงานพิเศษ (ปริญญานิพนธ์)</b></h2></center><br>
-            <center><h2><b class="font" style="font-size: 25px;">สาขาวิชาเทคโนโลยีสารสนเทศ ภาควิชาเทคโนโลยีสารสนเทศ</b></h2></center><br>
+            <center class="mg-bt-5px"><h2><b class="font" style="font-size: 25px;">แบบรายงานความก้าวหน้าของโครงงานพิเศษ (ปริญญานิพนธ์)</b></h2></center>
+            <center class="mg-bt-5px"><h2><b class="font" style="font-size: 25px;">สาขาวิชาเทคโนโลยีสารสนเทศ ภาควิชาเทคโนโลยีสารสนเทศ</b></h2></center>
             <center><h2><b class="font" style="font-size: 25px;">คณะเทคโนโลยีและการจัดการอุตสาหกรรม</b></h2></center><br>
           </div>
 
-        <div class="ReportCover">
-          <b class="font">ชื่อโครงการ</b>
-            <div class="font" style="margin-left:40px;"><b class="font">(ภาษาไทย)</b> {{projectSelected.thaiProjectName}}</div>
-            <div class="font" style="margin-left:40px;"><b class="font">(ภาษาอังกฤษ)</b> {{projectSelected.engProjectName}}</div> <br>
+          <div class="ReportCover">
+            <b class="font">ชื่อโครงการ</b>
+              <div class="font" style="margin-left:40px;"><b class="font">(ภาษาไทย)</b> {{projectSelected.thaiProjectName}}</div>
+              <div class="font" style="margin-left:40px;"><b class="font">(ภาษาอังกฤษ)</b> {{projectSelected.engProjectName}}</div><br>
 
-          <b class="font">กรณีศึกษา</b>
-            <div class="font" style="margin-left:40px;"><b class="font">(ภาษาไทย)</b> {{projectSelected.thaiCaseStudy?projectSelected.thaiCaseStudy:' -'}}</div>
-            <div class="font" style="margin-left:40px;"><b class="font">(ภาษาอังกฤษ)</b> {{projectSelected.engCaseStudy?projectSelected.engCaseStudy:' -'}}</div> <br><br><br>
+            <b class="font">กรณีศึกษา</b>
+              <div class="font" style="margin-left:40px;"><b class="font">(ภาษาไทย)</b> {{projectSelected.thaiCaseStudy?projectSelected.thaiCaseStudy:' -'}}</div>
+              <div class="font" style="margin-left:40px;"><b class="font">(ภาษาอังกฤษ)</b> {{projectSelected.engCaseStudy?projectSelected.engCaseStudy:' -'}}</div><br><br><br>
 
-          <b class="font">ชื่อนักศึกษาผู้ทำโครงงาน</b><br>
-          <div class="font" style="margin-left:40px;" :key="keyy" v-for="(member, keyy) in projectSelected.teams">
-            {{keyy+1}}. {{member.name}} {{member.lastname}}&nbsp;&nbsp;<b class="font">รหัสนักศึกษา</b> {{member.id}} <br>
-          </div><br><br><br>
+            <b class="font">ชื่อนักศึกษาผู้ทำโครงงาน</b><br>
+            <div class="font" style="margin-left:40px;" :key="keyy" v-for="(member, keyy) in projectSelected.teams">
+              {{keyy+1}}. {{member.name}} {{member.lastname}}&nbsp;&nbsp;<b class="font">รหัสนักศึกษา</b> {{member.id}} <br>
+            </div><br><br><br>
 
-          <b class="font">ชื่ออาจารย์ที่ปรึกษา / อาจารย์ที่ปรึกษาร่วม</b><br>
-            <div class="font" style="margin-left:40px;"> <b class="font"> อาจารย์ที่ปรึกษา</b>
-              {{project.mentor.position}}{{project.mentor.name}} {{project.mentor.lastname}}
+            <b class="font">ชื่ออาจารย์ที่ปรึกษา / อาจารย์ที่ปรึกษาร่วม</b><br>
+              <div class="font" style="margin-left:40px;"> <b class="font"> อาจารย์ที่ปรึกษา</b>
+                {{project.mentor.position}}{{project.mentor.name}} {{project.mentor.lastname}}
+            </div>
+              <div class="font" style="margin-left:40px;"> <b class="font"> อาจารย์ที่ปรึกษาร่วม</b>
+                {{projectSelected.coOpMentor?projectSelected.coOpMentor:' -'}}<br><br>
+            </div><br><br>
+
           </div>
-            <div class="font" style="margin-left:40px;"> <b class="font"> อาจารย์ที่ปรึกษาร่วม</b>
-              {{projectSelected.coOpMentor?projectSelected.coOpMentor:' -'}}<br><br>
-          </div><br><br>
-
         </div>
       </div>
     </div>
 <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
     <div style="display: flex; justify-content: center;" :key="key" v-for="(week, key) in project.scoreboard">
-      <div :id="'pdf'+key" class="box LayoutFrame">
-        <div class="NameProject">{{projectSelected.thaiProjectName}} &nbsp;&nbsp;หน้า {{key+1}}</div>
-        <!-- {{week.radio}} -->
-          <div>
-            <div class="content" style="font-family: 'Sarabun', sans-serif; font-size: 17px;">
-              <center><b class="font">รายงานความก้าวหน้าโครงงานพิเศษ ครั้งที่ {{key+1}}</b></center>
-              <center><b class="font">{{thaiDate(week.startDate)}}</b></center>
-              <br>
-              <br>
+      <div class="box bg-cl-white pd-40px mg-t-25px">
+        <div :id="'pdf'+key" class="LayoutFrame">
+          <div class="NameProject">{{projectSelected.thaiProjectName}} &nbsp;&nbsp;หน้า {{key+1}}</div>
+          <!-- {{week.radio}} -->
+            <div>
+              <div class="content" style="font-family: 'Sarabun', sans-serif; font-size: 17px;">
+                <center><b class="font">รายงานความก้าวหน้าโครงงานพิเศษ ครั้งที่ {{key+1}}</b></center>
+                <center><b class="font">{{thaiDate(week.startDate)}}</b></center>
+                <br>
 
-              <b class="font">1. เป้าหมายที่ตั้งไว้ในสัปดาห์นี้ (สัปดาห์ที่ {{key+1}})</b><br>
-              <div class="font" style="margin-left:20px">1.1 ความก้าวหน้า / ผลงานที่ดำเนินการมาแล้ว</div>
-              <div class="cut-text" style="margin-left:50px">
-                <b><div class="font">{{week.textProgress}}</div></b>
+
+                <b class="font">1. เป้าหมายที่ตั้งไว้ในสัปดาห์นี้ (สัปดาห์ที่ {{key+1}})</b><br>
+                <div class="font" style="margin-left:20px">1.1 ความก้าวหน้า / ผลงานที่ดำเนินการมาแล้ว</div>
+                <div class="cut-text" style="margin-left:50px">
+                  <b><div class="font">{{week.textProgress}}</div></b>
+                </div>
+                <br>
+
+
+                <div class="font" style="margin-left:20px">1.2 คิดเป็นร้อยละ &nbsp;<b class="font">{{week.progress?week.progress:'&nbsp;&nbsp;&nbsp;&nbsp;'}}</b> &nbsp;ของงานทั้งหมด</div>
+                <div class="font" style="margin-left:50px">จัดทำโครงงานได้
+                  <b v-if= "week.radio === 1" class="font">ตรงตามเป้าหมาย</b>
+                  <b v-else-if= "week.radio === 2" class="font">น้อยกว่าเป้าหมาย</b>
+                  <b v-else class="font"> &nbsp;   </b>
+                </div>
+                  <!-- ตรงตามเป้าหมาย / น้อยกกว่าเป้าหมาย -->
+                <br>
+
+
+                <div class="font" style="margin-left:20px">1.3 ปัญหาที่พบ</div>
+                <div class="cut-text" style="margin-left:50px">
+                  <b><div class="font">{{week.problem}}</div></b>
+                </div>
+                <br>
+
+
+                <div class="font" style="margin-left:20px">1.4 วิธีแก้ปัญหาที่พบ</div>
+                <div class="cut-text" style="margin-left:50px">
+                  <b><div class="font">{{week.solution}}</div></b>
+                </div>
+                <br>
+
+
+                <div class="font" style="display: flex; justify-content: flex-end; ">
+                  <b class="font">{{memberConfirm(week.membersSave)}}</b><br>
+                </div>
+                <div class="font" style="display: flex; justify-content: flex-end;">(ผู้จัดทำโครงการ)</div>
+
+                <div><b class="font">2. ความเห็นอาจารย์ที่ปรึกษา</b></div>
+                <div class="cut-text" style="margin-left:50px">
+                  <b><div class="font">{{week.mentorComment}}</div></b>
+                </div>
+
+                <div class="font" style="display: flex; justify-content: flex-end; ">
+                  <b class="font">{{week.membersSave?project.mentor.position:' '}}{{week.membersSave?project.mentor.name:' '}} {{week.membersSave?project.mentor.lastname:' '}}</b><br>
+                </div>
+                <div class="font" style="display: flex; justify-content: flex-end;">(อาจารย์ที่ปรึกษา)</div>
+
+                <div><b class="font">3. ความเห็นอาจารย์ประจำวิชา</b></div>
+                <div class="cut-text" style="margin-left:50px">
+                  <b><div class="font">{{week.subjectComment}}</div></b>
+                </div>
+
+                <div class="font" style="display: flex; justify-content: flex-end; ">
+                  <b class="font">{{week.membersSave? 'รับทราบ':' '}}</b><br>
+                </div>
+                <div class="font" style="display: flex; justify-content: flex-end;">(อาจารย์ประจำวิชา)</div>
+                <!-- <br><br><br><br><br><br><br><br><br><br><br><br> -->
+
               </div>
-              <br>
-              <br>
-
-              <div class="font" style="margin-left:20px">1.2 คิดเป็นร้อยละ &nbsp;<b class="font">{{week.progress?week.progress:'&nbsp;&nbsp;&nbsp;&nbsp;'}}</b> &nbsp;ของงานทั้งหมด</div>
-              <div class="font" style="margin-left:50px">จัดทำโครงงานได้
-                <b v-if= "week.radio === 1" class="font">ตรงตามเป้าหมาย</b>
-                <b v-else-if= "week.radio === 2" class="font">น้อยกว่าเป้าหมาย</b>
-                <b v-else class="font"> &nbsp;   </b>
-              </div>
-                <!-- ตรงตามเป้าหมาย / น้อยกกว่าเป้าหมาย -->
-              <br>
-              <br>
-
-              <div class="font" style="margin-left:20px">1.3 ปัญหาที่พบ</div>
-              <div class="cut-text" style="margin-left:50px">
-                <b><div class="font">{{week.problem}}</div></b>
-              </div>
-              <br>
-              <br>
-
-              <div class="font" style="margin-left:20px">1.4 วิธีแก้ปัญหาที่พบ</div>
-              <div class="cut-text" style="margin-left:50px">
-                <b><div class="font">{{week.solution}}</div></b>
-              </div>
-              <br>
-              <br>
-
-              <div class="font" style="display: flex; justify-content: flex-end; ">
-                <b class="font">{{memberConfirm(week.membersSave)}}</b><br>
-              </div>
-              <div class="font" style="display: flex; justify-content: flex-end;">(ผู้จัดทำโครงการ)</div>
-              <br>
-              <br>
-
-              <div><b class="font">2. ความเห็นอาจารย์ที่ปรึกษา</b></div>
-              <div class="cut-text" style="margin-left:50px">
-                <b><div class="font">{{week.mentorComment}}</div></b>
-              </div>
-              <br>
-              <br>
-
-              <div class="font" style="display: flex; justify-content: flex-end; ">
-                <b class="font">{{week.membersSave?project.mentor.position:' '}}{{week.membersSave?project.mentor.name:' '}} {{week.membersSave?project.mentor.lastname:' '}}</b><br>
-              </div>
-              <div class="font" style="display: flex; justify-content: flex-end;">(อาจารย์ที่ปรึกษา)</div>
-              <br>
-              <br>
-
-              <div><b class="font">3. ความเห็นอาจารย์ประจำวิชา</b></div>
-              <div class="cut-text" style="margin-left:50px">
-                <b><div class="font">{{week.subjectComment}}</div></b>
-              </div>
-              <br>
-
-              <div class="font" style="display: flex; justify-content: flex-end; ">
-                <b class="font">{{week.membersSave?'รับทราบ':' '}}</b><br>
-              </div>
-              <div class="font" style="display: flex; justify-content: flex-end;">(อาจารย์ประจำวิชา)</div>
-              <br><br><br><br><br><br><br><br><br><br><br><br>
-
             </div>
-          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -179,6 +175,7 @@ export default {
     },
     async gennerate (name, loadingComponent) {
       const docDefinition = {
+        pageMargins: [40, 20],
         content: [],
         defaultStyle: {
           font: 'THSarabunNew'
@@ -234,7 +231,6 @@ export default {
 
 <style scoped>
 .LayoutFrame{
-  margin-top: 25px;
   width: 870px;
   display: flex;
   justify-content: center;
@@ -264,7 +260,7 @@ export default {
   font-family: 'THSarabunNew', sans-serif;
 }
 .topTitle{
-  margin-left: 190px;
+  margin-left: 210px;
   margin-top: -175px;
 }
 .content{
@@ -275,10 +271,9 @@ export default {
   font-family: 'Sarabun', sans-serif;
 }
 .DownloadButton{
-  display: flex;
-  justify-content: flex-end;
-  margin-top: 10px;
-  margin-right: 60px;
+  position: fixed;
+  right: 20px;
+  bottom: 20px;
 }
 .cut-text {
   /* text-overflow: ellipsis; */
